5c5f5cc8ead5b6ac752a24a4a8de0bdefe594d66
melion11 <gphooenix@gmail.com>
2023-09-26 16:56:11 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import './styles/index.scss'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import { createRoot } from 'react-dom/client'
+
+import { App } from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(<App />)
